1f5b69b6e47eadad42886a55caa8df523e17b6bd
MavenW <ytsiuioh45@gmail.com>
2025-12-12 15:49:17 -0500

    Creacion de AdmClientte

---
+++ Controlador/AdmCliente.cs
@@ -0,0 +1,27 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controlador
+{
+        public class AdmCliente
+        {
+            static List<Cliente> listaCliente = new List<Cliente>();
+
+
+            // Método para obtener un cliente
+            public static Cliente ObtenerClientePorId(string cedula)
+            {
+                return listaCliente.Find(c => c.CedulaORuc == cedula);
+            }
+
+            // Método para obtener todos
+            public static List<Cliente> ObtenerTodosLosClientes()
+            {
+                return listaCliente;
+            }
+        }
+    }
