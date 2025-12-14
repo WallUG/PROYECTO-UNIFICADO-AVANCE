9e9e600449f78c54e747d1d216f87ad6a87904e4
Leonardo Plúas <leonardojrpluas@gmail.com>
2025-12-13 22:36:00 -0500

    V2

--- Vista/FrmRegistroInmueble.Designer.cs
+++ Vista/FrmRegistroInmueble.Designer.cs
@@ -54,54 +54,50 @@
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(160, 122);
-            this.txtNombre.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNombre.Location = new System.Drawing.Point(120, 99);
+            this.txtNombre.MaxLength = 200;
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(400, 22);
+            this.txtNombre.Size = new System.Drawing.Size(301, 20);
             this.txtNombre.TabIndex = 3;
             this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(20, 125);
-            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNombre.Location = new System.Drawing.Point(15, 102);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(59, 16);
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
             this.lblNombre.TabIndex = 2;
             this.lblNombre.Text = "Nombre:";
             // 
             // lblTipo
             // 
             this.lblTipo.AutoSize = true;
-            this.lblTipo.Location = new System.Drawing.Point(20, 35);
-            this.lblTipo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTipo.Location = new System.Drawing.Point(15, 28);
             this.lblTipo.Name = "lblTipo";
-            this.lblTipo.Size = new System.Drawing.Size(95, 16);
+            this.lblTipo.Size = new System.Drawing.Size(77, 13);
             this.lblTipo.TabIndex = 4;
             this.lblTipo.Text = "Tipo Inmueble:";
             // 
             // lblCantidad
             // 
             this.lblCantidad.AutoSize = true;
-            this.lblCantidad.Location = new System.Drawing.Point(20, 160);
-            this.lblCantidad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCantidad.Location = new System.Drawing.Point(15, 130);
             this.lblCantidad.Name = "lblCantidad";
-            this.lblCantidad.Size = new System.Drawing.Size(64, 16);
+            this.lblCantidad.Size = new System.Drawing.Size(52, 13);
             this.lblCantidad.TabIndex = 6;
             this.lblCantidad.Text = "Cantidad:";
             // 
             // nudCantidad
             // 
-            this.nudCantidad.Location = new System.Drawing.Point(160, 158);
-            this.nudCantidad.Margin = new System.Windows.Forms.Padding(4);
+            this.nudCantidad.Location = new System.Drawing.Point(120, 128);
             this.nudCantidad.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
             this.nudCantidad.Name = "nudCantidad";
-            this.nudCantidad.Size = new System.Drawing.Size(160, 22);
+            this.nudCantidad.Size = new System.Drawing.Size(120, 20);
             this.nudCantidad.TabIndex = 7;
             this.nudCantidad.Value = new decimal(new int[] {
             1,
@@ -112,24 +108,22 @@
             // nudPrecio
             // 
             this.nudPrecio.DecimalPlaces = 2;
-            this.nudPrecio.Location = new System.Drawing.Point(160, 194);
-            this.nudPrecio.Margin = new System.Windows.Forms.Padding(4);
+            this.nudPrecio.Location = new System.Drawing.Point(120, 158);
             this.nudPrecio.Maximum = new decimal(new int[] {
             5000,
             0,
             0,
             0});
             this.nudPrecio.Name = "nudPrecio";
-            this.nudPrecio.Size = new System.Drawing.Size(160, 22);
+            this.nudPrecio.Size = new System.Drawing.Size(120, 20);
             this.nudPrecio.TabIndex = 9;
             // 
             // lblPrecio
             // 
             this.lblPrecio.AutoSize = true;
-            this.lblPrecio.Location = new System.Drawing.Point(20, 196);
-            this.lblPrecio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPrecio.Location = new System.Drawing.Point(15, 159);
             this.lblPrecio.Name = "lblPrecio";
-            this.lblPrecio.Size = new System.Drawing.Size(95, 16);
+            this.lblPrecio.Size = new System.Drawing.Size(79, 13);
             this.lblPrecio.TabIndex = 8;
             this.lblPrecio.Text = "Precio Unitario:";
             // 
@@ -137,20 +131,18 @@
             // 
             this.cmbTipo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbTipo.FormattingEnabled = true;
-            this.cmbTipo.Location = new System.Drawing.Point(160, 32);
-            this.cmbTipo.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbTipo.Location = new System.Drawing.Point(120, 26);
             this.cmbTipo.Name = "cmbTipo";
-            this.cmbTipo.Size = new System.Drawing.Size(400, 24);
+            this.cmbTipo.Size = new System.Drawing.Size(301, 21);
             this.cmbTipo.TabIndex = 10;
             this.cmbTipo.SelectedIndexChanged += new System.EventHandler(this.selectTipoInmueble);
             // 
             // lblDisponibilidad
             // 
             this.lblDisponibilidad.AutoSize = true;
-            this.lblDisponibilidad.Location = new System.Drawing.Point(20, 234);
-            this.lblDisponibilidad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDisponibilidad.Location = new System.Drawing.Point(15, 190);
             this.lblDisponibilidad.Name = "lblDisponibilidad";
-            this.lblDisponibilidad.Size = new System.Drawing.Size(94, 16);
+            this.lblDisponibilidad.Size = new System.Drawing.Size(75, 13);
             this.lblDisponibilidad.TabIndex = 11;
             this.lblDisponibilidad.Text = "Disponibilidad:";
             // 
@@ -159,19 +151,17 @@
             this.chkDisponibilidad.AutoSize = true;
             this.chkDisponibilidad.Checked = true;
             this.chkDisponibilidad.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkDisponibilidad.Location = new System.Drawing.Point(160, 234);
-            this.chkDisponibilidad.Margin = new System.Windows.Forms.Padding(4);
+            this.chkDisponibilidad.Location = new System.Drawing.Point(120, 190);
             this.chkDisponibilidad.Name = "chkDisponibilidad";
-            this.chkDisponibilidad.Size = new System.Drawing.Size(18, 17);
+            this.chkDisponibilidad.Size = new System.Drawing.Size(15, 14);
             this.chkDisponibilidad.TabIndex = 14;
             this.chkDisponibilidad.UseVisualStyleBackColor = true;
             // 
             // btnGuardar
             // 
-            this.btnGuardar.Location = new System.Drawing.Point(240, 270);
-            this.btnGuardar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGuardar.Location = new System.Drawing.Point(180, 219);
             this.btnGuardar.Name = "btnGuardar";
-            this.btnGuardar.Size = new System.Drawing.Size(120, 37);
+            this.btnGuardar.Size = new System.Drawing.Size(90, 30);
             this.btnGuardar.TabIndex = 12;
             this.btnGuardar.Text = "Guardar";
             this.btnGuardar.UseVisualStyleBackColor = true;
@@ -179,23 +169,23 @@
             // 
             // txtContenido
             // 
-            this.txtContenido.Location = new System.Drawing.Point(20, 31);
-            this.txtContenido.Margin = new System.Windows.Forms.Padding(4);
+            this.txtContenido.Location = new System.Drawing.Point(15, 25);
             this.txtContenido.Multiline = true;
             this.txtContenido.Name = "txtContenido";
             this.txtContenido.ReadOnly = true;
             this.txtContenido.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtContenido.Size = new System.Drawing.Size(540, 150);
+            this.txtContenido.Size = new System.Drawing.Size(406, 123);
             this.txtContenido.TabIndex = 13;
             // 
             // txtDescripcion
             // 
             this.txtDescripcion.BackColor = System.Drawing.SystemColors.Info;
-            this.txtDescripcion.Location = new System.Drawing.Point(160, 64);
+            this.txtDescripcion.Location = new System.Drawing.Point(120, 52);
+            this.txtDescripcion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtDescripcion.Multiline = true;
             this.txtDescripcion.Name = "txtDescripcion";
             this.txtDescripcion.ReadOnly = true;
-            this.txtDescripcion.Size = new System.Drawing.Size(400, 50);
+            this.txtDescripcion.Size = new System.Drawing.Size(301, 41);
             this.txtDescripcion.TabIndex = 15;
             // 
             // contextMenuStrip1
@@ -208,10 +198,9 @@
             // 
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(150, 20);
-            this.lblTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitulo.Location = new System.Drawing.Point(112, 16);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(305, 31);
+            this.lblTitulo.Size = new System.Drawing.Size(277, 29);
             this.lblTitulo.TabIndex = 16;
             this.lblTitulo.Text = "Registro de Inmuebles";
             // 
@@ -229,11 +218,9 @@
             this.groupBoxDatos.Controls.Add(this.lblPrecio);
             this.groupBoxDatos.Controls.Add(this.nudPrecio);
             this.groupBoxDatos.Controls.Add(this.btnGuardar);
-            this.groupBoxDatos.Location = new System.Drawing.Point(16, 74);
-            this.groupBoxDatos.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBoxDatos.Location = new System.Drawing.Point(12, 60);
             this.groupBoxDatos.Name = "groupBoxDatos";
-            this.groupBoxDatos.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBoxDatos.Size = new System.Drawing.Size(580, 320);
+            this.groupBoxDatos.Size = new System.Drawing.Size(435, 260);
             this.groupBoxDatos.TabIndex = 17;
             this.groupBoxDatos.TabStop = false;
             this.groupBoxDatos.Text = "Datos del Inmueble";
@@ -241,25 +228,22 @@
             // groupBoxResultado
             // 
             this.groupBoxResultado.Controls.Add(this.txtContenido);
-            this.groupBoxResultado.Location = new System.Drawing.Point(16, 402);
-            this.groupBoxResultado.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBoxResultado.Location = new System.Drawing.Point(12, 327);
             this.groupBoxResultado.Name = "groupBoxResultado";
-            this.groupBoxResultado.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBoxResultado.Size = new System.Drawing.Size(580, 195);
+            this.groupBoxResultado.Size = new System.Drawing.Size(435, 158);
             this.groupBoxResultado.TabIndex = 18;
             this.groupBoxResultado.TabStop = false;
             this.groupBoxResultado.Text = "Resultado del Registro";
             // 
             // FrmRegistroInmueble
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(612, 612);
+            this.ClientSize = new System.Drawing.Size(459, 497);
             this.Controls.Add(this.groupBoxResultado);
             this.Controls.Add(this.groupBoxDatos);
             this.Controls.Add(this.lblTitulo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "FrmRegistroInmueble";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
