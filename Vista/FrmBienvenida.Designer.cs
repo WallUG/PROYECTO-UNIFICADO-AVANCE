52e64a0e4f964bc09e5bad9783785cbf4636049b
Leonardo Plúas <leonardojrpluas@gmail.com>
2025-12-13 21:36:13 -0500

    Menu edit

--- Vista/FrmBienvenida.Designer.cs
+++ Vista/FrmBienvenida.Designer.cs
@@ -30,40 +30,136 @@
         {
             this.btnOk = new System.Windows.Forms.Button();
             this.lblIngreso = new System.Windows.Forms.Label();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.panelContent = new System.Windows.Forms.Panel();
+            this.lblGrupo = new System.Windows.Forms.Label();
+            this.lblSistema = new System.Windows.Forms.Label();
+            this.lblSaludo = new System.Windows.Forms.Label();
+            this.panelHeader.SuspendLayout();
+            this.panelContent.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnOk
             // 
-            this.btnOk.Location = new System.Drawing.Point(219, 185);
+            this.btnOk.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.btnOk.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOk.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.ForeColor = System.Drawing.Color.White;
+            this.btnOk.Location = new System.Drawing.Point(240, 320);
+            this.btnOk.Margin = new System.Windows.Forms.Padding(4);
             this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.Size = new System.Drawing.Size(160, 45);
             this.btnOk.TabIndex = 1;
-            this.btnOk.Text = "OK";
-            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Text = "Continuar";
+            this.btnOk.UseVisualStyleBackColor = false;
             this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
             // 
             // lblIngreso
             // 
             this.lblIngreso.AutoSize = true;
-            this.lblIngreso.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblIngreso.Location = new System.Drawing.Point(143, 120);
+            this.lblIngreso.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIngreso.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.lblIngreso.Location = new System.Drawing.Point(50, 30);
+            this.lblIngreso.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblIngreso.Name = "lblIngreso";
-            this.lblIngreso.Size = new System.Drawing.Size(349, 29);
+            this.lblIngreso.Size = new System.Drawing.Size(0, 31);
             this.lblIngreso.TabIndex = 3;
-            this.lblIngreso.Text = "________________________";
+            this.lblIngreso.Visible = false;
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.panelHeader.Controls.Add(this.lblTitulo);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Margin = new System.Windows.Forms.Padding(4);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(640, 100);
+            this.panelHeader.TabIndex = 4;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.White;
+            this.lblTitulo.Location = new System.Drawing.Point(0, 0);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(640, 100);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "¡Bienvenido!";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelContent
+            // 
+            this.panelContent.BackColor = System.Drawing.Color.White;
+            this.panelContent.Controls.Add(this.lblGrupo);
+            this.panelContent.Controls.Add(this.lblSistema);
+            this.panelContent.Controls.Add(this.lblSaludo);
+            this.panelContent.Location = new System.Drawing.Point(50, 130);
+            this.panelContent.Margin = new System.Windows.Forms.Padding(4);
+            this.panelContent.Name = "panelContent";
+            this.panelContent.Size = new System.Drawing.Size(540, 170);
+            this.panelContent.TabIndex = 5;
+            // 
+            // lblGrupo
+            // 
+            this.lblGrupo.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGrupo.ForeColor = System.Drawing.Color.Gray;
+            this.lblGrupo.Location = new System.Drawing.Point(0, 130);
+            this.lblGrupo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblGrupo.Name = "lblGrupo";
+            this.lblGrupo.Size = new System.Drawing.Size(540, 30);
+            this.lblGrupo.TabIndex = 2;
+            this.lblGrupo.Text = "Grupo #4";
+            this.lblGrupo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSistema
+            // 
+            this.lblSistema.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSistema.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(73)))), ((int)(((byte)(94)))));
+            this.lblSistema.Location = new System.Drawing.Point(0, 70);
+            this.lblSistema.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSistema.Name = "lblSistema";
+            this.lblSistema.Size = new System.Drawing.Size(540, 60);
+            this.lblSistema.TabIndex = 1;
+            this.lblSistema.Text = "Sistema de Gestión de Eventos Sociales";
+            this.lblSistema.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSaludo
+            // 
+            this.lblSaludo.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSaludo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(128)))), ((int)(((byte)(185)))));
+            this.lblSaludo.Location = new System.Drawing.Point(0, 20);
+            this.lblSaludo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSaludo.Name = "lblSaludo";
+            this.lblSaludo.Size = new System.Drawing.Size(540, 40);
+            this.lblSaludo.TabIndex = 0;
+            this.lblSaludo.Text = "Hola, Usuario";
+            this.lblSaludo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FrmBienvenida
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(528, 271);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(240)))), ((int)(((byte)(241)))));
+            this.ClientSize = new System.Drawing.Size(640, 390);
+            this.Controls.Add(this.panelContent);
+            this.Controls.Add(this.panelHeader);
             this.Controls.Add(this.lblIngreso);
             this.Controls.Add(this.btnOk);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FrmBienvenida";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Bienvenid@";
+            this.Text = "Bienvenido al Sistema";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FrmBienvenida_FormClosed);
             this.Load += new System.EventHandler(this.FrmBienvenida_Load);
+            this.panelHeader.ResumeLayout(false);
+            this.panelContent.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,6 +168,12 @@
         #endregion
         private System.Windows.Forms.Button btnOk;
         private System.Windows.Forms.Label lblIngreso;
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Panel panelContent;
+        private System.Windows.Forms.Label lblSaludo;
+        private System.Windows.Forms.Label lblSistema;
+        private System.Windows.Forms.Label lblGrupo;
     }
 }
 
