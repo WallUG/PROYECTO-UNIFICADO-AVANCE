abb4e8129fdc2146e92d68e16ae164fea352ad4e
JOSUE RAFAEL CHICHANDA VELASQUEZ <josue.chichandavel@ug.edu.ec>
2026-01-21 19:11:55 -0500

    FrmEditar Implementado(Filtros y editaje)

--- Vista/FrmEditarCLiente.Designer.cs
+++ Vista/FrmEditarCLiente.Designer.cs
@@ -28,19 +28,287 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblEditarCliente = new System.Windows.Forms.Label();
+            this.lblFiltros = new System.Windows.Forms.Label();
+            this.txtBuscarxcedula = new System.Windows.Forms.TextBox();
+            this.lblBuscarxcedula = new System.Windows.Forms.Label();
+            this.tnFiltrar = new System.Windows.Forms.Button();
+            this.dgvCliente = new System.Windows.Forms.DataGridView();
+            this.colId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colApellidos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCedula = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colTelefono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCorreo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCorreoElectronico = new System.Windows.Forms.TextBox();
+            this.lblCorreoElectronico = new System.Windows.Forms.Label();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.lblTelefono = new System.Windows.Forms.Label();
+            this.TxtApellidos = new System.Windows.Forms.TextBox();
+            this.LblApellidos = new System.Windows.Forms.Label();
+            this.lllbNombres = new System.Windows.Forms.Label();
+            this.btnEditar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCliente)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lblEditarCliente
+            // 
+            this.lblEditarCliente.AutoSize = true;
+            this.lblEditarCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEditarCliente.Location = new System.Drawing.Point(506, 9);
+            this.lblEditarCliente.Name = "lblEditarCliente";
+            this.lblEditarCliente.Size = new System.Drawing.Size(255, 32);
+            this.lblEditarCliente.TabIndex = 0;
+            this.lblEditarCliente.Text = "EDITAR CLIENTE";
+            // 
+            // lblFiltros
+            // 
+            this.lblFiltros.AutoSize = true;
+            this.lblFiltros.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFiltros.Location = new System.Drawing.Point(270, 60);
+            this.lblFiltros.Name = "lblFiltros";
+            this.lblFiltros.Size = new System.Drawing.Size(112, 29);
+            this.lblFiltros.TabIndex = 27;
+            this.lblFiltros.Text = "FILTRO:";
+            // 
+            // txtBuscarxcedula
+            // 
+            this.txtBuscarxcedula.Location = new System.Drawing.Point(525, 103);
+            this.txtBuscarxcedula.MaxLength = 12;
+            this.txtBuscarxcedula.Name = "txtBuscarxcedula";
+            this.txtBuscarxcedula.Size = new System.Drawing.Size(236, 26);
+            this.txtBuscarxcedula.TabIndex = 26;
+            // 
+            // lblBuscarxcedula
+            // 
+            this.lblBuscarxcedula.AutoSize = true;
+            this.lblBuscarxcedula.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBuscarxcedula.Location = new System.Drawing.Point(298, 106);
+            this.lblBuscarxcedula.Name = "lblBuscarxcedula";
+            this.lblBuscarxcedula.Size = new System.Drawing.Size(207, 20);
+            this.lblBuscarxcedula.TabIndex = 25;
+            this.lblBuscarxcedula.Text = "BUSCAR POR CEDULA:\r\n";
+            // 
+            // tnFiltrar
+            // 
+            this.tnFiltrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tnFiltrar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tnFiltrar.Location = new System.Drawing.Point(811, 90);
+            this.tnFiltrar.Name = "tnFiltrar";
+            this.tnFiltrar.Size = new System.Drawing.Size(134, 52);
+            this.tnFiltrar.TabIndex = 24;
+            this.tnFiltrar.Text = "FILTRAR";
+            this.tnFiltrar.UseVisualStyleBackColor = true;
+            // 
+            // dgvCliente
+            // 
+            this.dgvCliente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCliente.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colId,
+            this.colNombre,
+            this.colApellidos,
+            this.colCedula,
+            this.colTelefono,
+            this.colCorreo});
+            this.dgvCliente.Location = new System.Drawing.Point(70, 212);
+            this.dgvCliente.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dgvCliente.Name = "dgvCliente";
+            this.dgvCliente.RowHeadersWidth = 51;
+            this.dgvCliente.RowTemplate.Height = 24;
+            this.dgvCliente.Size = new System.Drawing.Size(1191, 217);
+            this.dgvCliente.TabIndex = 28;
+            // 
+            // colId
+            // 
+            this.colId.FillWeight = 200F;
+            this.colId.Frozen = true;
+            this.colId.HeaderText = "ID";
+            this.colId.MinimumWidth = 6;
+            this.colId.Name = "colId";
+            this.colId.ReadOnly = true;
+            this.colId.Width = 150;
+            // 
+            // colNombre
+            // 
+            this.colNombre.FillWeight = 200F;
+            this.colNombre.HeaderText = "Nombre";
+            this.colNombre.MinimumWidth = 6;
+            this.colNombre.Name = "colNombre";
+            this.colNombre.ReadOnly = true;
+            this.colNombre.Width = 250;
+            // 
+            // colApellidos
+            // 
+            this.colApellidos.FillWeight = 200F;
+            this.colApellidos.HeaderText = "Apellidos";
+            this.colApellidos.MinimumWidth = 6;
+            this.colApellidos.Name = "colApellidos";
+            this.colApellidos.ReadOnly = true;
+            this.colApellidos.Width = 200;
+            // 
+            // colCedula
+            // 
+            this.colCedula.FillWeight = 200F;
+            this.colCedula.HeaderText = "Cedula";
+            this.colCedula.MinimumWidth = 6;
+            this.colCedula.Name = "colCedula";
+            this.colCedula.ReadOnly = true;
+            this.colCedula.Width = 200;
+            // 
+            // colTelefono
+            // 
+            this.colTelefono.FillWeight = 200F;
+            this.colTelefono.HeaderText = "Telefono cel";
+            this.colTelefono.MinimumWidth = 6;
+            this.colTelefono.Name = "colTelefono";
+            this.colTelefono.ReadOnly = true;
+            this.colTelefono.Width = 250;
+            // 
+            // colCorreo
+            // 
+            this.colCorreo.FillWeight = 200F;
+            this.colCorreo.HeaderText = "Correo";
+            this.colCorreo.MinimumWidth = 6;
+            this.colCorreo.Name = "colCorreo";
+            this.colCorreo.ReadOnly = true;
+            this.colCorreo.Width = 175;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(132, 547);
+            this.txtNombre.MaxLength = 100;
+            this.txtNombre.Multiline = true;
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(497, 46);
+            this.txtNombre.TabIndex = 38;
+            // 
+            // txtCorreoElectronico
+            // 
+            this.txtCorreoElectronico.Location = new System.Drawing.Point(132, 890);
+            this.txtCorreoElectronico.Multiline = true;
+            this.txtCorreoElectronico.Name = "txtCorreoElectronico";
+            this.txtCorreoElectronico.Size = new System.Drawing.Size(437, 41);
+            this.txtCorreoElectronico.TabIndex = 37;
+            // 
+            // lblCorreoElectronico
+            // 
+            this.lblCorreoElectronico.AutoSize = true;
+            this.lblCorreoElectronico.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCorreoElectronico.Location = new System.Drawing.Point(142, 843);
+            this.lblCorreoElectronico.Name = "lblCorreoElectronico";
+            this.lblCorreoElectronico.Size = new System.Drawing.Size(274, 25);
+            this.lblCorreoElectronico.TabIndex = 36;
+            this.lblCorreoElectronico.Text = "CORREO ELECTRONICO*";
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(132, 766);
+            this.txtTelefono.MaxLength = 10;
+            this.txtTelefono.Multiline = true;
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(336, 41);
+            this.txtTelefono.TabIndex = 35;
+            // 
+            // lblTelefono
+            // 
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTelefono.Location = new System.Drawing.Point(142, 717);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(137, 25);
+            this.lblTelefono.TabIndex = 34;
+            this.lblTelefono.Text = "TELEFONO*";
+            // 
+            // TxtApellidos
+            // 
+            this.TxtApellidos.Location = new System.Drawing.Point(132, 642);
+            this.TxtApellidos.MaxLength = 100;
+            this.TxtApellidos.Multiline = true;
+            this.TxtApellidos.Name = "TxtApellidos";
+            this.TxtApellidos.Size = new System.Drawing.Size(497, 46);
+            this.TxtApellidos.TabIndex = 31;
+            // 
+            // LblApellidos
+            // 
+            this.LblApellidos.AutoSize = true;
+            this.LblApellidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblApellidos.Location = new System.Drawing.Point(142, 596);
+            this.LblApellidos.Name = "LblApellidos";
+            this.LblApellidos.Size = new System.Drawing.Size(126, 25);
+            this.LblApellidos.TabIndex = 30;
+            this.LblApellidos.Text = "APELLIDO*";
+            // 
+            // lllbNombres
+            // 
+            this.lllbNombres.AutoSize = true;
+            this.lllbNombres.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lllbNombres.Location = new System.Drawing.Point(142, 487);
+            this.lllbNombres.Name = "lllbNombres";
+            this.lllbNombres.Size = new System.Drawing.Size(113, 25);
+            this.lllbNombres.TabIndex = 29;
+            this.lllbNombres.Text = "NOMBRE*";
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEditar.Location = new System.Drawing.Point(834, 749);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(127, 70);
+            this.btnEditar.TabIndex = 39;
+            this.btnEditar.Text = "EDITAR";
+            this.btnEditar.UseVisualStyleBackColor = true;
             // 
             // FrmEditarCLiente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1381, 825);
+            this.ClientSize = new System.Drawing.Size(1381, 1120);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCorreoElectronico);
+            this.Controls.Add(this.lblCorreoElectronico);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.TxtApellidos);
+            this.Controls.Add(this.LblApellidos);
+            this.Controls.Add(this.lllbNombres);
+            this.Controls.Add(this.dgvCliente);
+            this.Controls.Add(this.lblFiltros);
+            this.Controls.Add(this.txtBuscarxcedula);
+            this.Controls.Add(this.lblBuscarxcedula);
+            this.Controls.Add(this.tnFiltrar);
+            this.Controls.Add(this.lblEditarCliente);
             this.Name = "FrmEditarCLiente";
             this.Text = "FrmEditarCLiente";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCliente)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblEditarCliente;
+        private System.Windows.Forms.Label lblFiltros;
+        private System.Windows.Forms.TextBox txtBuscarxcedula;
+        private System.Windows.Forms.Label lblBuscarxcedula;
+        private System.Windows.Forms.Button tnFiltrar;
+        private System.Windows.Forms.DataGridView dgvCliente;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colApellidos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCedula;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colTelefono;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCorreo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCorreoElectronico;
+        private System.Windows.Forms.Label lblCorreoElectronico;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.Label lblTelefono;
+        private System.Windows.Forms.TextBox TxtApellidos;
+        private System.Windows.Forms.Label LblApellidos;
+        private System.Windows.Forms.Label lllbNombres;
+        private System.Windows.Forms.Button btnEditar;
     }
 }
